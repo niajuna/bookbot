6055c379992650de6bdadc5ee914f3d0f76e8499
niajuna <rachie.niajuna@gmail.com>
2025-11-09 16:10:51 +0900

    get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(file_path):
+    """ファイルパスから本の内容を読み取る関数
+    Args:
+        file_path (str): 本が含まれるテキストファイルのパス
+    Returns:
+        str: 本の内容を文字列として返す
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+def main():
+    book_path = 'books/frankenstein.txt'
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+main()
